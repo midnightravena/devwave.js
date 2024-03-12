3dd2c05fdddbfacf2ae443ee098cc6ad8e91723b
Sahil <151955025+sahilarun@users.noreply.github.com>
2024-03-12 18:40:28 +0530

    Update index.ts

--- lib/index.ts
+++ lib/index.ts
@@ -2,3 +2,4 @@ export type * from "./types/index";
 
 export { default as वाहिन्यः } from "./structures/Channel";
 export { default as संविचारः } from "./structures/Interaction";
+export { default as प्रकटनानालयः } from "./structures/AnnouncementChannel";
