6797d1d09ab2abbff2845de62db1ea526c56711a
Sahil <151955025+sahilarun@users.noreply.github.com>
2024-03-13 11:43:16 +0530

    Create DiscordHTTPError.ts

---
+++ lib/rest/DiscordHTTPError.ts
@@ -0,0 +1,4 @@
+/** @module DiscordHTTPत्रुटिः */
+import DiscordRESTत्रुटिः from "./DiscordRESTError";
+import type { विश्रमणविधि } from "../Constants";
+import type { JSONDiscordHTTPत्रुटिः } from "../types/json";
